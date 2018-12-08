562cbbebca20c1f1ab5a2d85c80d56107aca926d
Luma <tomorinao.info@gmail.com>
2018-12-08 17:09:17 +0900

    fix ternary-search lib

--- src/algorithm/ternary-search.cpp
+++ src/algorithm/ternary-search.cpp
@@ -15,21 +15,24 @@ T ternary_search(T lo, T hi, Func f, int c, bool maximize = 0) {
   for(int i = 0; i < c; i++) {
     T m1 = lo + (hi - lo) / 3;
     T m2 = lo + (hi - lo) * 2 / 3;
-    if(maximize ? f(m1) > f(m2) : f(m1) < f(m2)) hi = m2;
+    if(maximize ? f(m1) > f(m2) : f(m1) < f(m2))
+      hi = m2;
+    else
+      lo = m1;
   }
   return (lo + hi) / 2;
 }
 
-// needed iteration : log((hi - lo) / eps) / log((phi + 1) / phi)
+// needed iteration : log((hi - lo) / eps) / log(phi)
 #include <cmath>
 template < class T, class Func >
 T golden_search(T lo, T hi, Func f, int c, bool maximize = 0) {
-  constexpr double phi = (1 + sqrt(5)) / 2;
+  const double phi_inv = 2 / (1 + sqrt(5));
   T v1, v2;
   bool f1 = 1, f2 = 1;
   for(int i = 0; i < c; i++) {
-    T m1 = lo + (hi - lo) / (phi + 1);
-    T m2 = lo + (hi - lo) * phi / (phi + 1);
+    T m1 = lo + (hi - lo) * (phi_inv * phi_inv);
+    T m2 = lo + (hi - lo) * phi_inv;
     v1 = f1 ? f(m1) : v1;
     v2 = f2 ? f(m2) : v2;
     f1 = f2 = 0;
