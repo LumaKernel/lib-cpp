90f3ea496e6e9a84fd462a85d63b004fc4173f65
Luma <tomorinao.info@gmail.com>
2018-12-22 20:32:22 +0900

    modify mincostflow lib

--- src/graph/flow/MinCostFlow.cpp
+++ src/graph/flow/MinCostFlow.cpp
@@ -5,8 +5,21 @@ using ll = long long;
 
 // @@
 // @snippet     mincostflow
-// @name MinCostFlow Library
-/// --- MinCostFlow Library {{{ ///
+// @name MinCostFlow
+
+// MinCostFlow(size, inf)
+// MinCostFlow(size, inf, int& s, int& t)
+// addEdge(from, to, capacity, cost, undirected = false)
+// === build(s, t, flow) ===
+// O(FE log V)
+// return mincost
+// return -1 if impossible
+/// --- MinCostFlow {{{ ///
+#include <cassert>
+#include <queue>
+#include <tuple>
+#include <vector>
+// with Dijkstra with Potential
 struct MinCostFlow {
   struct Edge {
     int to;
@@ -15,46 +28,61 @@ struct MinCostFlow {
   };
   int n;
   vector< vector< Edge > > graph;
+  vector< ll > h; // potential
   vector< ll > dist;
-  vector< int > prevv, previ;
+  vector< int > prevv, preve;
   ll inf;
-  MinCostFlow(int n, ll inf) : n(n), graph(n), dist(n), prevv(n), previ(n), inf(inf) {}
+  MinCostFlow(int n, ll inf)
+      : n(n), graph(n), h(n), dist(n), prevv(n), preve(n), inf(inf) {}
+  MinCostFlow(int n, ll inf, int &s, int &t) : MinCostFlow(n + 2, inf) {
+    s = n, t = n + 1;
+  }
   void addEdge(int a, int b, ll cap, ll cost, int undirected = 0) {
+    assert(cost >= 0);
+    assert(0 <= a && a < n);
+    assert(0 <= b && b < n);
     graph[a].emplace_back((Edge){b, cap, cost, graph[b].size()});
     graph[b].emplace_back((Edge){a, undirected ? cap : 0, -cost, graph[a].size() - 1});
   }
-  ll solve(int s, int t, ll f) {
+  ll build(int s, int t, ll f) {
+    h.assign(n, 0);
     ll res = 0;
     while(f > 0) {
-      fill(begin(dist), end(dist), inf);
+      dist.assign(n, inf);
       dist[s] = 0;
-      bool update = 1;
-      // Bellman-Ford
-      while(update) {
-        update = 0;
-        for(int v = 0; v < n; v++) {
-          if(dist[v] == inf) continue;
-          for(size_t i = 0; i < graph[v].size(); i++) {
-            Edge &edge = graph[v][i];
-            if(edge.cap > 0 && dist[edge.to] > dist[v] + edge.cost) {
-              dist[edge.to] = dist[v] + edge.cost;
-              prevv[edge.to] = v;
-              previ[edge.to] = i;
-              update = 1;
-            }
+      // Dijkstra with potential
+      using P = pair< ll, int >;
+      priority_queue< P, vector< P >, greater< P > > pq;
+      pq.emplace(0, s);
+      while(pq.size()) {
+        ll d;
+        int v;
+        tie(d, v) = pq.top();
+        pq.pop();
+        if(dist[v] < d) continue;
+        for(size_t i = 0; i < graph[v].size(); i++) {
+          Edge &edge = graph[v][i];
+          ll nd = dist[v] + edge.cost + h[v] - h[edge.to];
+          if(edge.cap > 0 && dist[edge.to] > nd) {
+            dist[edge.to] = nd;
+            pq.emplace(nd, edge.to);
+            prevv[edge.to] = v;
+            preve[edge.to] = i;
           }
         }
       }
       //
       if(dist[t] == inf) return -1;
+      for(int v = 0; v < n; v++) h[v] += dist[v];
+
       ll d = f;
       for(int v = t; v != s; v = prevv[v]) {
-        d = min(d, graph[prevv[v]][previ[v]].cap);
+        d = min(d, graph[prevv[v]][preve[v]].cap);
       }
       f -= d;
-      res += d * dist[t];
+      res += d * h[t];
       for(int v = t; v != s; v = prevv[v]) {
-        Edge &edge = graph[prevv[v]][previ[v]];
+        Edge &edge = graph[prevv[v]][preve[v]];
         edge.cap -= d;
         graph[v][edge.rev].cap += d;
       }
@@ -62,7 +90,6 @@ struct MinCostFlow {
     return res;
   }
 };
-
 /// }}}--- ///
 
 // const int N = 100000;
--- src/graph/flow/MinCostFlow.md
+++ src/graph/flow/MinCostFlow.md
@@ -2,6 +2,22 @@
 title: 最小費用流
 ---
 
-未検証，蟻本の写しただけ．（1問TLEあり，たぶん動く）
+ポテンシャルを用いたDijkstraをしている (TODO : 勉強中)
 
-// @ MinCostFlow Library
+$O(FE \log V)$
+
+// @ MinCostFlow
+
+# 検証
+
+* [G - Sum of Cards](https://atcoder.jp/contests/code-thanks-festival-2018-open/submissions/3836949){:target="_blank"}<!--_-->
+
+# 練習問題
+
+なし
+
+# 参考
+
+* 蟻本pp.199-204
+
+
