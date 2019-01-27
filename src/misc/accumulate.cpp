33c5607f3395ecfd9e23b48bd500f4cf0f3c0b12
Luma <tomorinao.info@gmail.com>
2019-01-27 16:31:46 +0900

    add range1D lib

--- src/misc/accumulate.cpp
+++ src/misc/accumulate.cpp
@@ -18,13 +18,12 @@ void accum2D(T &val, int h, int w) {
 }
 
 template < class T >
-ll range2D(T &val, int y1, int x1, int y2, int x2, int h, int w) {
-  if(y1 < 0) y1 = 0;
-  if(x1 < 0) x1 = 0;
+auto range2D(T &val, int y1, int x1, int y2, int x2, int h, int w) {
+  using V = decltype(val[0][0]);
   if(y2 >= h) y2 = h - 1;
   if(x2 >= w) x2 = w - 1;
-  if(y1 > y2 || x1 > x2) return ll(0);
-  ll res(val[y2][x2]);
+  if(y1 > y2 || x1 > x2) return V(0);
+  V res(val[y2][x2]);
   if(y1 - 1 >= 0) res -= val[y1 - 1][x2];
   if(x1 - 1 >= 0) res -= val[y2][x1 - 1];
   if(y1 - 1 >= 0 && x1 - 1 >= 0) res += val[y1 - 1][x1 - 1];
@@ -32,8 +31,9 @@ ll range2D(T &val, int y1, int x1, int y2, int x2, int h, int w) {
 }
 
 template < class T >
-ll range2Dloop(T &val, ll ty1, ll tx1, ll y2, ll x2, int h, int w) {
-  if(ty1 > y2 || tx1 > x2) return ll(0);
+auto range2Dloop(T &val, ll ty1, ll tx1, ll y2, ll x2, int h, int w) {
+  using V = decltype(val[0][0]);
+  if(ty1 > y2 || tx1 > x2) return V(0);
 
   ll y1 = ty1 % h;
   if(y1 < 0) y1 += h;
@@ -48,7 +48,7 @@ ll range2Dloop(T &val, ll ty1, ll tx1, ll y2, ll x2, int h, int w) {
   y2 %= h;
   x2 %= w;
 
-  ll res(0);
+  V res(0);
   if(gy == 0 && gx == 0) {
     res += range2D< T >(val, y1, x1, y2, x2, h, w);
   } else if(gy == 0) {
@@ -71,5 +71,69 @@ ll range2Dloop(T &val, ll ty1, ll tx1, ll y2, ll x2, int h, int w) {
     res += range2D< T >(val, 0, 0, h - 1, w - 1, h, w) * (gy - 1) * (gx - 1); // C
   }
   return res;
+}
+// }}}
+
+// @new
+// @ Accumulation 1D Utility
+// @snippet range1d
+// @alias accum1d 1d_range_accum loop_range1d
+
+// accum1D, range1D, range1Dloop {{{
+#include<vector>
+template<class T>
+void accum1D(T &a, int n) {
+  for(int i = 1; i < n; i++) a[i] += a[i-1];
+}
+template<class T>
+void accum1D(vector<T> &a) {
+  accum1D(a, a.size());
+}
+template<class T, int N>
+void accum1D(T a[N]) {
+  accum1D(a, N);
+}
+
+template<class T>
+auto range1D(const T &a, int l, int r, int n) {
+  if(r >= n) r = n - 1;
+  auto res(a[r]);
+  if(l - 1 >= 0) res -= a[l - 1];
+  return res;
+}
+template<class T>
+auto range1D(vector<T> &a, int l, int r) {
+  return range1D(a, l, r, a.size());
+}
+template<class T, int N>
+auto range1D(T a[N], int l, int r) {
+  return range1D(a, l, r, N);
+}
+
+template<class T>
+auto range1Dloop(const T &a, ll tl, ll r, int n) {
+  decltype(a[0]) res(0);
+  if(tl > r) return res;
+  assert(n >= 1);
+  ll l = tl % n;
+  if(l < 0) l += n;
+  r += l - tl;
+  ll gr = r / n;
+  if(gr == 0) {
+    res += range1D<T>(a, l, r, n);
+  } else {
+    res += range1D<T>(a, l, n - 1, n);
+    res += range1D<T>(a, 0, r % n, n);
+    res += range1D<T>(a, 0, n - 1, n) * (gr - 1);
+  }
+  return res;
+}
+template<class T>
+T range1Dloop(vector<T> &a, ll l, ll r) {
+  return range1Dloop(a, l, r, a.size());
+}
+template<class T, int N>
+T range1Dloop(T a[N], ll l, ll r) {
+  return range1Dloop(a, l, r, N);
 }
 // }}}
