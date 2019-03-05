bde30c4a4e6474470e0c2c2f400291c841398cb1
Luma <tomorinao.info@gmail.com>
2019-03-06 03:34:02 +0900

    modify accumulate lib

--- src/misc/accumulate.cpp
+++ src/misc/accumulate.cpp
@@ -1,14 +1,10 @@
-// @import header
-// #include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-
-// @@
 // @ Accumulation 2D Utility
 // @snippet range2d
 // @alias accum2d 2d_range_accum loop_range2d
 
 // accum2D range2D range2Dloop {{{
+#include <type_traits>
+using loop2d_int = long long;
 template < class T >
 void accum2D(T &val, int h, int w) {
   for(int i = 1; i < h; i++)
@@ -17,12 +13,12 @@ void accum2D(T &val, int h, int w) {
     for(int j = 1; j < w; j++) val[i][j] += val[i][j - 1];
 }
 
-template < class T >
-auto range2D(T &val, int y1, int x1, int y2, int x2, int h, int w) {
-  using V = decltype(val[0][0]);
+template < class T,
+           class V = typename std::remove_reference< decltype(T()[0][0]) >::type >
+V range2D(T &val, int y1, int x1, int y2, int x2, int h, int w) {
   if(y2 >= h) y2 = h - 1;
   if(x2 >= w) x2 = w - 1;
-  if(y1 > y2 || x1 > x2) return V(0);
+  if(y1 > y2 || x1 > x2) return static_cast< V >(0);
   V res(val[y2][x2]);
   if(y1 - 1 >= 0) res -= val[y1 - 1][x2];
   if(x1 - 1 >= 0) res -= val[y2][x1 - 1];
@@ -30,21 +26,22 @@ auto range2D(T &val, int y1, int x1, int y2, int x2, int h, int w) {
   return res;
 }
 
-template < class T >
-auto range2Dloop(T &val, ll ty1, ll tx1, ll y2, ll x2, int h, int w) {
-  using V = decltype(val[0][0]);
-  if(ty1 > y2 || tx1 > x2) return V(0);
+template < class T,
+           class V = typename std::remove_reference< decltype(T()[0][0]) >::type >
+auto range2Dloop(T &val, loop2d_int ty1, loop2d_int tx1, loop2d_int y2, loop2d_int x2,
+                 int h, int w) {
+  if(ty1 > y2 || tx1 > x2) return static_cast< V >(0);
 
-  ll y1 = ty1 % h;
+  loop2d_int y1 = ty1 % h;
   if(y1 < 0) y1 += h;
-  ll x1 = tx1 % w;
+  loop2d_int x1 = tx1 % w;
   if(x1 < 0) x1 += w;
 
   y2 += y1 - ty1;
   x2 += x1 - tx1;
 
-  ll gy = y2 / h;
-  ll gx = x2 / w;
+  loop2d_int gy = y2 / h;
+  loop2d_int gx = x2 / w;
   y2 %= h;
   x2 %= w;
 
@@ -80,13 +77,15 @@ auto range2Dloop(T &val, ll ty1, ll tx1, ll y2, ll x2, int h, int w) {
 // @alias accum1d 1d_range_accum loop_range1d
 
 // accum1D, range1D, range1Dloop {{{
+#include <cassert>
 #include <vector>
+using loop1d_int = long long;
 template < class T >
 void accum1D(T &a, int n) {
   for(int i = 1; i < n; i++) a[i] += a[i - 1];
 }
 template < class T >
-void accum1D(vector< T > &a) {
+void accum1D(std::vector< T > &a) {
   accum1D(a, a.size());
 }
 template < class T, int N >
@@ -102,7 +101,7 @@ auto range1D(const T &a, int l, int r, int n) {
   return res;
 }
 template < class T >
-auto range1D(vector< T > &a, int l, int r) {
+auto range1D(std::vector< T > &a, int l, int r) {
   return range1D(a, l, r, a.size());
 }
 template < class T, int N >
@@ -111,14 +110,15 @@ auto range1D(T a[N], int l, int r) {
 }
 
 template < class T >
-auto range1Dloop(const T &a, ll tl, ll r, int n) {
-  decltype(a[0]) res(0);
+auto range1Dloop(const T &a, loop1d_int tl, loop1d_int r, int n) {
+  using V = typename std::remove_reference< decltype(a[0]) >::type;
+  V res(0);
   if(tl > r) return res;
   assert(n >= 1);
-  ll l = tl % n;
+  loop1d_int l = tl % n;
   if(l < 0) l += n;
   r += l - tl;
-  ll gr = r / n;
+  loop1d_int gr = r / n;
   if(gr == 0) {
     res += range1D< T >(a, l, r, n);
   } else {
@@ -129,11 +129,11 @@ auto range1Dloop(const T &a, ll tl, ll r, int n) {
   return res;
 }
 template < class T >
-T range1Dloop(vector< T > &a, ll l, ll r) {
+T range1Dloop(std::vector< T > &a, loop1d_int l, loop1d_int r) {
   return range1Dloop(a, l, r, a.size());
 }
 template < class T, int N >
-T range1Dloop(T a[N], ll l, ll r) {
+T range1Dloop(T a[N], loop1d_int l, loop1d_int r) {
   return range1Dloop(a, l, r, N);
 }
 // }}}
